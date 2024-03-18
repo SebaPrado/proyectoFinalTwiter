81b04a8e27a45efafe6b3398621d40cb3f7f924d
PabloAbreuMinasian <157127354+PabloAbreuMinasian@users.noreply.github.com>
2024-03-18 11:38:04 -0300

    cambio de token

--- src/redux/UserSlice.js
+++ src/redux/UserSlice.js
@@ -3,7 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: " user",
   initialState: {
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWQ2MTUwMmUwZTc5Y2M3MjMyZWU5NTEiLCJpYXQiOjE3MTA3MTI5NDd9.zHbsLPznh1_eGJ2G6YJRC1dn179pO2MvS2nwQei_qM0",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWY4NGRkNGQ0ZTJkZGM3NWI0NGZhY2IiLCJpYXQiOjE3MTA3NzE4MzV9.Rt8XtrlzPFG0tmCRZ7B5pqr7BX380ArocTtKwdN0hBQ",
   },
   reducers: {
     updateToken(state, action) {
@@ -14,14 +15,13 @@ const userSlice = createSlice({
   },
 });
 
-
 // updateToken= actions.updateToken
 // const userReducer = userSlice.reducer;
 // const actions = userSlice.actions;
 
-// export const { userAuthorization, updateToken }  
+// export const { userAuthorization, updateToken }
 // export default userReducer;
 
-const {reducer, actions}= userSlice;
-export const {updateToken}= actions
-export default reducer 
+const { reducer, actions } = userSlice;
+export const { updateToken } = actions;
+export default reducer;
