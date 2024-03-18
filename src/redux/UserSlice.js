45def2b982d1bcc87a7c8dd34ca43af4b6fcc086
SebaPrado <sebastian.pradomelesi@gmail.com>
2024-03-18 13:04:56 -0300

    token vacio

--- src/redux/UserSlice.js
+++ src/redux/UserSlice.js
@@ -3,12 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: " user",
   initialState: {
-<<<<<<< HEAD
     token: "",
-=======
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWY4NGRkNGQ0ZTJkZGM3NWI0NGZhY2IiLCJpYXQiOjE3MTA3NzE4MzV9.Rt8XtrlzPFG0tmCRZ7B5pqr7BX380ArocTtKwdN0hBQ",
->>>>>>> b3ee9b08bc781649a1dfbdf825ece3756bff19f1
   },
   reducers: {
     updateToken(state, action) {
